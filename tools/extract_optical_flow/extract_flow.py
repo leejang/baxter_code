5a9591dafcc5bbafa8573f1952aab4ffd3ccef88
Jangwon Lee <leejang@madthunder.soic.indiana.edu>
2018-07-14 12:08:43 -0400

    Added: modified Caffe files based on the standard SSD Caffe

--- tools/extract_optical_flow/extract_flow.py
+++ tools/extract_optical_flow/extract_flow.py
@@ -4,6 +4,7 @@ import glob
 import re
 import os
 
+# 1. Hhow to use
 """
 frame1 = cv2.imread('0001.jpg')
 frame2 = cv2.imread('0002.jpg')
@@ -30,6 +31,7 @@ cv2.imwrite('flow_y.jpg',vert)
 cv2.imwrite('flow_xy.jpg',rgb)
 """
 
+# to sort files according to file name with numbers
 pat = re.compile("(\d+)\D*$")
 def key_func(x):
     mat=pat.search(os.path.split(x)[-1]) # match last group of digits
@@ -37,52 +39,93 @@ def key_func(x):
         return x
     return "{:>10}".format(mat.group(1)) # right align to 10 digits.
 
-int_cnt = 1
-for cur_img in sorted(glob.glob("/fast-data/leejang/egohands_data/Test/JPEGImages/*.jpg"), key=key_func):
+# to find files in all sub directories
+def list_files(dir):                                                                                                  
+    r = []                                                                                                            
+    subdirs = [x[0] for x in os.walk(dir)]                                                                            
+    for subdir in subdirs:                                                                                            
+        files = os.walk(subdir).next()[2]                                                                             
+        if (len(files) > 0):                                                                                          
+            for file in files:                                                                                        
+                r.append(subdir + "/" + file)                                                                         
+    return r
 
-    file_name = os.path.basename(cur_img)
 
-    #print cur_img
-    #print file_name
-    base_name = os.path.splitext(file_name)[0]
-    print base_name
+# extract optical flow
+def extract_flow(path, key_func):
 
-    if int_cnt == 1:
-      frame1 = cv2.imread(cur_img)
-      prev_name = base_name
-    else:
-      frame2 = cv2.imread(cur_img)
+    int_cnt = 1
+    file_path = path + "/*.jpg"
 
-      prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
-      next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
-      hsv = np.zeros_like(frame1)
-      hsv[...,1] = 255
+    print file_path
+    for cur_img in sorted(glob.glob(file_path), key=key_func):
 
-      flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-      mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-      hsv[...,0] = ang*180/np.pi/2
-      hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-      rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+        file_name = os.path.basename(cur_img)
 
-      horz = cv2.normalize(flow[...,0], None, 0, 255, cv2.NORM_MINMAX)     
-      vert = cv2.normalize(flow[...,1], None, 0, 255, cv2.NORM_MINMAX)
-      horz = horz.astype('uint8')
-      vert = vert.astype('uint8')
+        #print cur_img
+        #print file_name
 
-      # save optical flow files
-      flow_x = '/fast-data/leejang/egohands_data/Test/Flows/flow_x/flow_x_'+prev_name+'.jpg'
-      flow_y = '/fast-data/leejang/egohands_data/Test/Flows/flow_y/flow_y_'+prev_name+'.jpg'
-      flow_xy = '/fast-data/leejang/egohands_data/Test/Flows/flow_xy/flow_xy_'+prev_name+'.jpg'
+        base_name = os.path.splitext(file_name)[0]
+        print base_name
 
-      cv2.imwrite(flow_x, horz)
-      cv2.imwrite(flow_y, vert)
-      cv2.imwrite(flow_xy, rgb)
+        if int_cnt == 1:
+            frame1 = cv2.imread(cur_img)
+            prev_name = base_name
+        else:
+            frame2 = cv2.imread(cur_img)
 
-      # update the previous frame
-      frame1 = cv2.imread(cur_img)
-      prev_name = base_name
+            prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+            next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+            hsv = np.zeros_like(frame1)
+            hsv[...,1] = 255
 
-    int_cnt += 1
+            flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+            hsv[...,0] = ang*180/np.pi/2
+            hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+            rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+
+            horz = cv2.normalize(flow[...,0], None, 0, 255, cv2.NORM_MINMAX)     
+            vert = cv2.normalize(flow[...,1], None, 0, 255, cv2.NORM_MINMAX)
+            horz = horz.astype('uint8')
+            vert = vert.astype('uint8')
+
+            # save optical flow files
+            flow_x = path + '/flow_x_' + prev_name + '.jpg'
+            flow_y = path + '/flow_y_' + prev_name + '.jpg'
+            flow_xy = path + '/flow_xy_' + prev_name + '.jpg'
+
+            cv2.imwrite(flow_x, horz)
+            cv2.imwrite(flow_y, vert)
+            cv2.imwrite(flow_xy, rgb)
+
+            # update the previous frame
+            frame1 = cv2.imread(cur_img)
+            prev_name = base_name
+
+        # increase cnt
+        int_cnt += 1
+
+
+###############################################
+# Main
+###############################################
+target_dir = "/data/leejang/recorded_videos_on_0830_2016/scenario1"
+
+subdirs = [x[0] for x in os.walk(target_dir)]
+
+for subdir in subdirs:
+
+    path, folder = os.path.split(subdir)
+
+    #print path
+    #print folder
+    if folder.isdigit():
+      print subdir
+      print "---------------------"
+      # extract optical flow
+      extract_flow(subdir, key_func)
+
 
 
 print("done!!")
